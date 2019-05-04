3fc0d3066b86a3f57121ee333ea350751e849905
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-04 08:53:52 +0200

    style Key component using styled-components

--- src/components/Sequencer/Track/TrackPanel/CellSettings/NoteSelector/Key.js
+++ src/components/Sequencer/Track/TrackPanel/CellSettings/NoteSelector/Key.js
@@ -1,5 +1,7 @@
 // @flow strict
 import * as React from "react"
+// $FlowFixMe
+import styled from "styled-components/macro"
 
 import Color from "../../../../../../utils/color/colorLibrary"
 
@@ -19,6 +21,21 @@ type Props = {
 type State = {
   hover: boolean
 }
+
+const KeyStyled = styled.button`
+  display: inline-block;
+  height: 100%;
+  width: ${props => props.width}px;
+  background-color: ${props =>
+    Key.backgroundColor(props.color, props.black, props.active, props.hover)};
+  padding: 0;
+  border: 1px solid
+    ${props =>
+      props.black ? Color.get900(props.color) : Color.get200(props.color)};
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+  cursor: ${props => (props.active ? "default" : "pointer")};
+`
 
 class Key extends React.Component<Props, State> {
   state = {
@@ -52,30 +69,9 @@ class Key extends React.Component<Props, State> {
   }
 
   render() {
-    const css = {
-      display: "inline-block",
-      height: "100%",
-      width: `${this.props.width}px`,
-      backgroundColor: Key.backgroundColor(
-        this.props.color,
-        this.props.black,
-        this.props.active,
-        this.state.hover
-      ),
-      padding: 0,
-      border: `1px solid ${
-        this.props.black
-          ? Color.get900(this.props.color)
-          : Color.get200(this.props.color)
-      }`,
-      borderBottomLeftRadius: "3px",
-      borderBottomRightRadius: "3px",
-      cursor: this.props.active ? "default" : "pointer"
-    }
-
     return (
-      <button
-        style={css}
+      <KeyStyled
+        {...this.props}
         onClick={() => this.props.onClick()}
         onMouseEnter={() => {
           this.hoverOn()
@@ -87,9 +83,49 @@ class Key extends React.Component<Props, State> {
         }}
       >
         {" "}
-      </button>
+      </KeyStyled>
     )
   }
+
+  // render() {
+  //   const css = {
+  //     display: "inline-block",
+  //     height: "100%",
+  //     width: `${this.props.width}px`,
+  //     backgroundColor: Key.backgroundColor(
+  //       this.props.color,
+  //       this.props.black,
+  //       this.props.active,
+  //       this.state.hover
+  //     ),
+  //     padding: 0,
+  //     border: `1px solid ${
+  //       this.props.black
+  //         ? Color.get900(this.props.color)
+  //         : Color.get200(this.props.color)
+  //     }`,
+  //     borderBottomLeftRadius: "3px",
+  //     borderBottomRightRadius: "3px",
+  //     cursor: this.props.active ? "default" : "pointer"
+  //   }
+  //
+  //   return (
+  //     <button
+  //       style={css}
+  //       onClick={() => this.props.onClick()}
+  //       onMouseEnter={() => {
+  //         this.hoverOn()
+  //         this.props.onHoverStart()
+  //       }}
+  //       onMouseLeave={() => {
+  //         this.hoverOff()
+  //         this.props.onHoverStop()
+  //       }}
+  //     >
+  //       {" "}
+  //     </button>
+  //   )
+  // }
 }
 
 export default Key
