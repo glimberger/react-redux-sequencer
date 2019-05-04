7a756a60252ff6d3a89d84f86619c340ebe07df4
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-04 22:17:55 +0200

    add a quarter note grid

--- src/components/Sequencer/Sequencer.js
+++ src/components/Sequencer/Sequencer.js
@@ -26,6 +26,96 @@ const prefs = {
   }
 }
 
+const StyledSequencer = styled.div`
+  background-color: transparent;
+  background-image: ${({ prefs }) => `linear-gradient(
+    90deg,
+    transparent ${prefs.panel.width + prefs.gutter / 2.0 - 1}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width + prefs.gutter / 2.0 - 1}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width + prefs.gutter / 2.0 + 1}px,
+    transparent ${prefs.panel.width + prefs.gutter / 2.0 + 1}px
+  ),
+  linear-gradient(
+    90deg,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter)}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter)}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter)}px,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter)}px
+  ),
+  linear-gradient(
+    90deg,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 2}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 2}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 2}px,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 2}px
+  ),
+  linear-gradient(
+    90deg,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 3}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 3}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 3}px,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 3}px
+  ),
+  linear-gradient(
+    90deg,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 4}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 -
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 4}px,
+    rgba(255, 255, 255, 0.2) ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 4}px,
+    transparent ${prefs.panel.width +
+      prefs.gutter / 2.0 +
+      1 +
+      (8 * prefs.cellSize + 8 * prefs.gutter) * 4}px
+  )
+  `};
+
+  background-size: 100%;
+`
+
 const Row = styled.div`
   margin-top: ${({ first, gutter }) => (first ? 0 : gutter)}px;
 `
@@ -37,18 +127,20 @@ const AddTrackWrapper = styled.div`
 
 function Sequencer({ orderedTracks }: Props) {
   return (
-    <React.Fragment>
-      {orderedTracks.map((track, idx) => (
-        <Row key={track.id} gutter={prefs.gutter} first={idx === 0}>
-          <TrackWithConnect
-            trackID={track.id}
-            panelWidth={prefs.panel.width}
-            panelHeight={prefs.panel.height}
-            gutter={prefs.gutter}
-            cellSize={prefs.cellSize}
-          />
-        </Row>
-      ))}
+    <div>
+      <StyledSequencer prefs={prefs}>
+        {orderedTracks.map((track, idx) => (
+          <Row key={track.id} gutter={prefs.gutter} first={idx === 0}>
+            <TrackWithConnect
+              trackID={track.id}
+              panelWidth={prefs.panel.width}
+              panelHeight={prefs.panel.height}
+              gutter={prefs.gutter}
+              cellSize={prefs.cellSize}
+            />
+          </Row>
+        ))}
+      </StyledSequencer>
       <Row gutter={prefs.gutter} first={false}>
         <AddTrackWrapper>
           <AddTrack
@@ -60,7 +152,7 @@ function Sequencer({ orderedTracks }: Props) {
           />
         </AddTrackWrapper>
       </Row>
-    </React.Fragment>
+    </div>
   )
 }
 
