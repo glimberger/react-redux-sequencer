af835c7ef908f5ec92c35ea976900933934dc649
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-13 10:07:50 +0200

    refactor Knob component style using styled-components

--- src/components/controllers/Knob/Knob.js
+++ src/components/controllers/Knob/Knob.js
@@ -1,13 +1,14 @@
 // @flow strict
 import * as React from "react"
+// $FlowFixMe
+import styled from "styled-components/macro"
 
-import styles from "./Knob.module.css"
 import { coordinates } from "../../../utils/trigo/polar"
 import Color from "../../../utils/color/colorLibrary"
 
 import type { MaterialColor } from "../../../utils/color/colorLibrary"
 
-type PropsType = {
+type Props = {
   value: number,
   onChange: (value: number) => void,
   min: number,
@@ -16,18 +17,36 @@ type PropsType = {
   prefs: { color: MaterialColor, size: number }
 }
 
-function Knob({ value, min, max, step, prefs, onChange }: PropsType) {
+const StyledKnob = styled.div`
+  position: relative;
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+`
+
+const StyledSvg = styled.svg`
+  pointer-events: none;
+  width: 100%;
+  height: 100%;
+`
+
+const StyledInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  cursor: grab;
+  width: 100%;
+  height: 100%;
+  transform: rotate(-90deg);
+  opacity: 0;
+`
+
+function Knob({ value, min, max, step, prefs, onChange }: Props) {
   const angle = (360 / (1.0 * (max - min))) * (parseFloat(value) - min)
 
   const { x, y } = coordinates(35, 35, 35)(angle)
-
   return (
-    <div
-      className={styles.Container}
-      style={{ width: `${prefs.size}px`, height: `${prefs.size}px` }}
-    >
-      <svg
-        className={styles.Visual}
+    <StyledKnob width={prefs.size} height={prefs.size}>
+      <StyledSvg
         viewBox="0 0 70 70"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -46,13 +65,12 @@ function Knob({ value, min, max, step, prefs, onChange }: PropsType) {
           <circle cx="35" cy="35" r="29" fill="#21252B" />
           <circle cx="35" cy="56" r="4" fill={Color.getA100(prefs.color)} />
         </g>
-      </svg>
-      <input
+      </StyledSvg>
+      <StyledInput
         type="range"
         min={min}
         max={max}
         step={step}
-        className={styles.Input}
         value={value}
         onChange={(e: SyntheticInputEvent<HTMLInputElement>) => {
           const { value } = e.currentTarget
@@ -60,8 +78,7 @@ function Knob({ value, min, max, step, prefs, onChange }: PropsType) {
           onChange(parseFloat(value))
         }}
       />
-      <div>{angle}</div>
-    </div>
+    </StyledKnob>
   )
 }
 
