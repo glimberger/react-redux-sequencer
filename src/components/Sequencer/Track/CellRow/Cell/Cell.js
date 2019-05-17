63257a2098140ea35f3b445779f94aff0a21d331
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-17 22:33:15 +0200

    refactor: set Cell Component functional

--- src/components/Sequencer/Track/CellRow/Cell/Cell.js
+++ src/components/Sequencer/Track/CellRow/Cell/Cell.js
@@ -24,20 +24,58 @@ type Props = {
   onClick: () => void
 }
 
-type State = {
+const cellWidth = (
+  resolution: NoteResolution,
+  size: number,
+  margin: number
+): number => {
+  if (resolution === 1) {
+    return (size + margin) / 2.0 - margin
+  }
+  if (resolution === 2) {
+    return size
+  }
+  if (resolution === 4) {
+    return (size + margin) * 2.0 - margin
+  }
+
+  return size
+}
+
+const borderColor = (
+  color: MaterialColor,
+  edited: boolean,
+  played: boolean,
+  scheduled: boolean,
   hover: boolean
+): string => {
+  if (edited) {
+    return hover ? Color.get50(color) : Color.get100(color)
+  }
+
+  if (played) {
+    return hover ? Color.get100(color) : Color.getA100(color)
+  }
+
+  if (scheduled) {
+    return hover ? Color.getA100(color) : Color.getA700(color)
+  }
+
+  return hover
+    ? Color.getA100(color)
+    : colorLuminance(Color.getA700(color), -0.4)
 }
 
 const StyledCell = styled.button`
   cursor: pointer;
   height: ${({ size }) => size}px;
   width: ${({ noteResolution, size, gutter }) =>
-    Cell.cellWidth(noteResolution, size, gutter)}px;
+    cellWidth(noteResolution, size, gutter)}px;
   margin-right: ${({ gutter }) => gutter}px;
   padding: 0;
   border: 3px solid
-    ${({ color, edited, played, scheduled, hover }) =>
-      Cell.borderColor(color, edited, played, scheduled, hover)};
+    ${({ color, edited, played, scheduled }) =>
+      borderColor(color, edited, played, scheduled, false)};
   border-radius: 3px;
   background-color: ${({ scheduled, hover, color, gain }) => {
     const rgb = hexToRgb(Color.getA700(color))
@@ -49,99 +87,42 @@ const StyledCell = styled.button`
           }, ${gain})`
       : "transparent"
   }};
+
+  &:hover {
+    border-color: ${({ color, edited, played, scheduled }) =>
+      borderColor(color, edited, played, scheduled, true)};
+  }
 `
 
-class Cell extends React.Component<Props, State> {
-  static defaultProps = {
-    edited: false,
-    gutter: 0
+Cell.defaultProps = {
+  edited: false,
+  gutter: 0
+}
+
+function Cell(props: Props) {
+  const buttonRef = React.createRef<HTMLButtonElement>()
+
+  const handleClick = () => {
+    props.onClick()
+    buttonRef.current && buttonRef.current.blur()
   }
 
-  state = {
-    hover: false
-  }
-
-  buttonRef = React.createRef<HTMLButtonElement>()
-
-  hoverOn() {
-    this.setState({ hover: true })
-  }
-  hoverOff() {
-    this.setState({ hover: false })
-  }
-
-  static cellWidth(
-    resolution: NoteResolution,
-    size: number,
-    margin: number
-  ): number {
-    if (resolution === 1) {
-      return (size + margin) / 2.0 - margin
-    }
-    if (resolution === 2) {
-      return size
-    }
-    if (resolution === 4) {
-      return (size + margin) * 2.0 - margin
-    }
-
-    return size
-  }
-
-  static borderColor(
-    color: MaterialColor,
-    edited: boolean,
-    played: boolean,
-    scheduled: boolean,
-    hover: boolean
-  ): string {
-    if (edited) {
-      return hover ? Color.get50(color) : Color.get100(color)
-    }
-
-    if (played) {
-      return hover ? Color.get100(color) : Color.getA100(color)
-    }
-
-    if (scheduled) {
-      return hover ? Color.getA100(color) : Color.getA700(color)
-    }
-
-    return hover
-      ? Color.getA100(color)
-      : colorLuminance(Color.getA700(color), -0.4)
-  }
-
-  handleClick() {
-    this.props.onClick()
-    this.buttonRef.current && this.buttonRef.current.blur()
-  }
-
-  render() {
-    console.assert(
-      typeof this.props.processing.gain.gain === "number",
-      this.props.processing.gain.gain
-    )
-    return (
-      <StyledCell
-        color={this.props.color}
-        size={this.props.size}
-        gutter={this.props.gutter}
-        noteResolution={this.props.noteResolution}
-        gain={this.props.processing.gain.gain}
-        played={this.props.played}
-        scheduled={this.props.scheduled}
-        edited={this.props.edited}
-        hover={this.state.hover}
-        ref={this.buttonRef}
-        onClick={() => this.handleClick()}
-        onMouseEnter={() => this.hoverOn()}
-        onMouseLeave={() => this.hoverOff()}
-      >
-        {" "}
-      </StyledCell>
-    )
-  }
+  return (
+    <StyledCell
+      color={props.color}
+      size={props.size}
+      gutter={props.gutter}
+      noteResolution={props.noteResolution}
+      gain={props.processing.gain.gain}
+      played={props.played}
+      scheduled={props.scheduled}
+      edited={props.edited}
+      ref={buttonRef}
+      onClick={() => handleClick()}
+    >
+      {" "}
+    </StyledCell>
+  )
 }
 
 export default Cell
