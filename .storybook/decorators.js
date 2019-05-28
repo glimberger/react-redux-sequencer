874576505c4067237f13d4d6ab3c24d0b045ad39
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-28 22:03:07 +0200

    refactor(redux): set named exports for storybook decorators in storybook directory

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -3,6 +3,29 @@ import { Provider } from "react-redux"
 import { createStore } from "redux"
 
 import rootReducer from "../src/redux/reducers"
+import { PrefsProvider } from "../src/components/context/sequencer-prefs"
+import configureStore from "../src/redux/store/configureStore"
+import type { AppState } from "../src/redux/store/configureStore"
+
+export const withContainer = story => (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      padding: "1rem",
+      backgroundColor: "#1e1f26"
+    }}
+  >
+    {story()}
+  </div>
+)
+
+export const withPrefsProvider = story => <PrefsProvider>{story()}</PrefsProvider>
+
+export const withReduxProvider = (state: AppState) => {
+  const store = configureStore(state)
+  return story => <Provider store={store}>{story()}</Provider>
+}
 
 export const reduxDecorator = (state = undefined) => {
   const initialSate = {
