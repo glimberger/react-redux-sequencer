f7bcc5297f486a7fe0af95ba4ff6002247569ead
Guillaume Limberger <guillaume.limberger@lecnam.net>
2019-05-29 17:12:54 +0200

    refactor: update TrackPanel stories
    use custom decorators

--- src/components/Sequencer/Track/TrackPanel/TrackPanel.stories.js
+++ src/components/Sequencer/Track/TrackPanel/TrackPanel.stories.js
@@ -1,23 +1,62 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 
-import { reduxDecorator } from "../../../../../.storybook/decorators"
+import {
+  withContainer,
+  withPrefsProvider,
+  withReduxProvider
+} from "../../../../../.storybook/decorators"
 
 import TrackPanel from "./TrackPanel"
+import { initialSate } from "../../../../redux/reducers"
+import colorLibrary from "../../../../utils/color/colorLibrary"
+
+const mapping = Array.from(Array(128).keys()).map(note => ({
+  sampleID: "1",
+  detune: 0
+}))
+
+const matrix = Array.from(Array(64).keys()).map(beat => ({
+  scheduled: beat % 3 === 0,
+  midi: 69,
+  processing: { gain: { gain: 1 } }
+}))
+
+const state = {
+  ...initialSate,
+  audio: {
+    ...initialSate.audio,
+    currentBeat: 0
+  },
+  session: {
+    ...initialSate.session,
+    activeTrackID: "1",
+    activeCellBeat: 6,
+    trackOrder: ["1"],
+    tracks: {
+      "1": {
+        instrumentID: '1',
+        color: colorLibrary.LIGHT_BLUE,
+        noteResolution: 2,
+        processing: { gain: { gain: 0.7 } }
+      }
+    },
+    instruments: {
+      "1": { id: '1', mapping }
+    },
+    matrix: {
+      "1": matrix
+    },
+    samples: {
+      "1": {label: "Sample"}
+    }
+  }
+}
 
 storiesOf("TrackPanel", module)
-  .addDecorator(reduxDecorator())
-  .addDecorator(story => (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        backgroundColor: "#1e1f26"
-      }}
-    >
-      {story()}
-    </div>
-  ))
-  .add("connected with redux", () => (
-    <TrackPanel cellSize={36} headerWidth={300} height={300} gutter={6} />
+  .addDecorator(story => withReduxProvider(state)(story))
+  .addDecorator(story => withPrefsProvider(story))
+  .addDecorator(story => withContainer(story))
+  .add("default", () => (
+    <TrackPanel />
   ))
